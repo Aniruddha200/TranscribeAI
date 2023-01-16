26ff9ef24794017e25cda5d4cd2e579b87b1246e
Aniruddha200 <aniruddha200baidya@protonmail.com>
2023-01-16 12:24:44 +0530

    Initial Commit

---
+++ TranscribeAI/TranscribeAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranscribeAIApp.swift
+//  TranscribeAI
+//
+//  Created by ABPersonal on 16/01/23.
+//
+
+import SwiftUI
+
+@main
+struct TranscribeAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
